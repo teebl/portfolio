efb6bb48455dbb95bf88fc338a46c85def5ff605
teebl <trevor.seibel@gmail.com>
2019-04-02 19:09:12 -0400

    Updates

---
+++ precache-manifest.d98e0a068c24bd78e6e3f549ce5ee8d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b90c7d178ba7c4f8c657",
+    "url": "/portfolio/static/js/main.102cdf31.chunk.js"
+  },
+  {
+    "revision": "de301a47ba343699e8c6",
+    "url": "/portfolio/static/js/2.423868cc.chunk.js"
+  },
+  {
+    "revision": "b90c7d178ba7c4f8c657",
+    "url": "/portfolio/static/css/main.282777f7.chunk.css"
+  },
+  {
+    "revision": "8611daeda6e943eeb4c195acf8e0aa0b",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b8c1cbe63ec400b689d2163b9b921fd7.js"
+  "/portfolio/precache-manifest.d98e0a068c24bd78e6e3f549ce5ee8d3.js"
 );
 
 workbox.clientsClaim();
